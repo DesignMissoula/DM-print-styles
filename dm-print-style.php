6ccc0350b787263fe5eadfdcd44339ad8dc25877
Bradford Knowlton <brad@ucanbonline.net>
2016-02-10 14:44:02 -0700

    initial commit with header file

---
+++ dm-print-style.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: DM Print Style
+Description: Printer Friendly Style Sheets
+Plugin URI: http://designmissoula.com
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+Version: 1.0.1
+License: GPL2
+Text Domain: dm
+*/
+
+/*
+
+    Copyright (C) 2016  Bradford Knowlton  brad@ucanbonline.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
